277bb7428a18b7997bf09475503b73ee8ad3b397
tommy-lean <k-tomilin99@mail.ru>
2022-11-23 18:16:42 +0500

    Implement add function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,15 @@
  * @link http:/devonline.academy/java
  */
 public class HelloWorld {
+    int c = 3;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 5;
+        int b = 3;
+
+        System.out.println(add(a, b));
+    }
+
+    private static int add(int a, int b) {
+        return a + b;
     }
 }
