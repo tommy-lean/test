876d5de77ff55b4ecf2bf1bd8e4cf6451ab1b8c5
tommy-lean <k-tomilin99@mail.ru>
2022-11-23 19:54:22 +0500

    Implement min function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,6 +13,7 @@ public class HelloWorld {
         System.out.println(div(a, b));
         System.out.println(mod(a, b));
         System.out.println(pow(a, b));
+        System.out.println(min(a, b));
 
     }
 
@@ -38,6 +39,10 @@ public class HelloWorld {
 
     private static int pow(int a, int b) {
         return (int) Math.pow(a, b);
+    }
+
+    private static int min(int a, int b) {
+        return Math.min(a, b);
     }
 
 
